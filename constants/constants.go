bef768f5a20a74e9e48a691ae04e1c4c93ba4ae6
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2026-07-09 19:15:24 -0600

    update cli version

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,6 @@
 package constants
 
-const CLI_VERSION = "0.0.11"
+const CLI_VERSION = "1.0.2"
 const TOKEN_FILE = "token.txt"
 const LOCAL_AGENT_UUID_FILE = "local_agent_uuid.txt"
 const LOCAL_AGENT_TOKEN_FILE = "local_agent_token.txt"
