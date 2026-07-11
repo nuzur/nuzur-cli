827533b76b5e0fe0175f1748150e026597a2ce1c
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2026-07-11 11:03:02 -0600

    bump version

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,6 @@
 package constants
 
-const CLI_VERSION = "1.0.4"
+const CLI_VERSION = "1.0.5"
 const TOKEN_FILE = "token.txt"
 const LOCAL_AGENT_UUID_FILE = "local_agent_uuid.txt"
 const LOCAL_AGENT_TOKEN_FILE = "local_agent_token.txt"
